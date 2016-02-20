212efd0a641ffa5936bfeac634f73aef75323a08
Breein <grig.cem@gmail.com>
2016-02-20 20:05:47 +0600

    Библиотека для работы с LS.

---
+++ us/js/ls.js
@@ -0,0 +1,36 @@
+function LocalStorage(){
+
+}
+
+LocalStorage.prototype = {
+  /**
+   * @param {string} key
+   * @param {object} object
+   */
+  save: function(key, object){
+    if(typeof object == "object"){
+      object = JSON.stringify(object);
+      localStorage.setItem(key, object);
+    }
+  },
+
+  /**
+   * @param {string} key
+   * @returns {{}}
+   */
+  load: function(key){
+    var object;
+
+    object = localStorage.getItem(key);
+    if(object){
+      return JSON.parse(object);
+    }else{
+      this.save(key, {});
+      return {};
+    }
+  }
+};
+
+module.exports = function(){
+  return new LocalStorage();
+};
