b61d29bcd9bd975eee83c8727d53ce49fb0fa43e
Breein <grig.cem@gmail.com>
2016-03-20 01:22:24 +0600

    Переработал код, так что всегда послдений аргумент - узел события.

--- us/js/events.js
+++ us/js/events.js
@@ -1,16 +1,21 @@
-module.exports = function bindEvent(element, event, callback) {
-  if (!element) {
-    return;
+module.exports = function bindEvent(element, event, callback, args) {
+  if (!element) return;
+
+  if(args == null){
+    args = [element];
+  }else{
+    args.push(element);
   }
-  if (element.addEventListener) {
-    if (event.substr(0, 2) == 'on') {
+
+  if(element.addEventListener){
+    if(event.substr(0, 2) == 'on'){
       event = event.substr(2);
     }
-    element.addEventListener(event, callback, false);
-  } else if (element.attachEvent) {
-    if (event.substr(0, 2) != 'on') {
-      event = 'on'+event;
+    element.addEventListener(event, ()=>{callback.apply(this, args)}, false);
+  }else if(element.attachEvent){
+    if(event.substr(0, 2) != 'on'){
+      event = 'on' + event;
     }
-    element.attachEvent(event, callback, false);
+    element.attachEvent(event, ()=>{callback.apply(this, args)}, false);
   }
 };
