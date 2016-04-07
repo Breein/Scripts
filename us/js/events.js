701b284c7d789f7aeb2d9b221906ae8d45fdd2cd
Breein <grig.cem@gmail.com>
2016-04-08 01:24:44 +0600

    Добавлен контекст. Надо фиксить методы с евентом. В других скриптах.

--- us/js/events.js
+++ us/js/events.js
@@ -1,20 +1,21 @@
-module.exports = function bindEvent(element, textEvent, callback, args, e) {
+module.exports = function bindEvent(element, textEvent, callback, args, context, e) {
   var index; if (!element) return;
 
   args == null ? args = [element] : args.push(element);
+  if(!context) context = null;
   index = args.length;
 
   if(element.addEventListener){
     if(textEvent.substr(0, 2) == 'on') textEvent = textEvent.substr(2);
     element.addEventListener(textEvent, (event)=>{
       if(e != null) args[index] = event;
-      callback.apply(null, args);
+      callback.apply(context, args);
     }, false);
   }else if(element.attachEvent){
     if(textEvent.substr(0, 2) != 'on') textEvent = 'on' + textEvent;
     element.attachEvent(textEvent, (event)=>{
       if(e != null) args[index] = event;
-      callback.apply(null, args);
+      callback.apply(context, args);
     }, false);
   }
 };
