66d32aa8226442a52088c4d5dbf110924b5d4f5b
Breein <grig.cem@gmail.com>
2016-06-15 22:28:54 +0600

    Фикс.

--- us/scripts/Watchdog/gulpfile.js
+++ us/scripts/Watchdog/gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('wrap', function(){
 
   userScriptDir = 'Watchdog_[GW]';
   userScriptName = userScriptDir + '.user.js';
-  userScriptVersion = '1.03';
+  userScriptVersion = '1.031';
 
   nowDate = new Date(new Date().getTime()).toLocaleString();
   userScriptHeader =
