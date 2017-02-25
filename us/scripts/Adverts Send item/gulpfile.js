1d16436aa629b210f60ef43c18a582a38f4fc8da
Breein <grig.cem@gmail.com>
2017-02-26 01:26:48 +0700

    Версия +

--- us/scripts/Adverts Send item/gulpfile.js
+++ us/scripts/Adverts Send item/gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('wrap', function(){
 
   userScriptDir = 'Adverts._Send_item_[GW]';
   userScriptName = 'Adverts._Send_item_[GW].user.js';
-  userScriptVersion = '1.13';
+  userScriptVersion = '1.131';
 
   nowDate = new Date(new Date().getTime()).toLocaleString();
   userScriptHeader =
